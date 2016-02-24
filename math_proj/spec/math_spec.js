492edb6c6de7ac3368fdaa5155a923f2a087d8be
unknown <sharpepj@SL-337-08.ad.jmu.edu>
2016-02-24 11:00:01 -0500

    Added multiplication and division with appropriate tests

--- math_proj/spec/math_spec.js
+++ math_proj/spec/math_spec.js
@@ -10,4 +10,13 @@ describe("A simple arithmetic program", function() {
   it("can subtract two numbers", function() {
   	expect(math.subtract(3,5)).toBe(-2);
   });
+
+  it("can multiply two numbers", function() {
+  	expect(math.multiply(3,5)).toBe(15);
+  });
+
+  it("can divide two numbers", function() {
+  	expect(math.divide(3,5)).toBe(0.6);
+  });
+
 });
