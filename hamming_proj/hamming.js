d88c436c87ff77287524e43f0991ea9019a4621e
unknown <sharpepj@dukes.jmu.edu>
2016-03-30 10:24:57 -0400

    Maybe fixed hamming submodule

---
+++ hamming_proj/hamming.js
@@ -0,0 +1,22 @@
+var compute;
+
+compute = function(strand1, strand2) {
+	var count = 0;
+	if(strand1.length === strand2.length)
+	{
+		for(var i = 0; i < strand1.length; i++) {
+			if(strand1.charAt(i) != strand2.charAt(i)) {
+				count++;
+			}
+		}
+
+	} else {
+		throw new Error('DNA strands must be of equal length.');
+	}
+	return count;
+}
+
+
+module.exports = {
+	compute: compute
+};
